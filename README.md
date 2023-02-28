57c010ad45c7473fa0ce6abc062f0eaa6e97d645
kazokmr <engkokmr@gmail.com>
2023-03-01 08:47:10 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Spring for GraphQL Sample Project
+
+Spring公式の[ガイド](https://spring.io/guides/gs/graphql-server/) をベースにした学習用プロジェクトです。
---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.3"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of("19"))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework:spring-webflux")
+    testImplementation("org.springframework.graphql:spring-graphql-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
