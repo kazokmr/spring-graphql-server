d727d23e86efb5cd60f234245197056e3606c35b
kazokmr <engkokmr@gmail.com>
2023-03-04 07:06:50 +0900

    Mutationのテストを追加 ※一部不明なアサーションがありコメントアウトしている

--- src/test/java/com/example/springgraphqlserver/controller/BookControllerTest.java
+++ src/test/java/com/example/springgraphqlserver/controller/BookControllerTest.java
@@ -1,9 +1,14 @@
 package com.example.springgraphqlserver.controller;
 
+import com.example.springgraphqlserver.form.UpdateBookPayload;
+import graphql.Assert;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
+
+import java.util.Map;
 
 @GraphQlTest(BookController.class)
 class BookControllerTest {
@@ -12,6 +17,7 @@ class BookControllerTest {
     private GraphQlTester graphQlTester;
 
     @Test
+    @DisplayName("１つ目の本が取得できる")
     void shouldGetFirstBook() {
         this.graphQlTester
                 // 指定したファイル名およびqueryのariasを指定する
@@ -42,6 +48,46 @@ class BookControllerTest {
 //                .entity(Author.class)
 //                .matches(author -> author.firstName().equals("Joshua"))
 //                .matches(author -> author.lastName().equals("Block"));
+    }
 
+    @Test
+    @DisplayName("登録した本がレスポンスに渡されること")
+    void addBook() {
+        this.graphQlTester
+                // クエリを直接定義する
+                .document("""
+                        mutation addBook($input: AddingBookInput!){
+                            addBook(input: $input) {
+                                book{
+                                    id
+                                    name
+                                    pageCount
+                                    author {
+                                        firstName
+                                        lastName
+                                    }
+                                }
+                            }
+                        }
+                        """)
+                // 変数の設定。Input型の値はMapでセットする
+                .variable("input",
+                        Map.of("name", "test",
+                                "pageCount", 528,
+                                "author_firstName", "T",
+                                "author_lastName", "O"
+                        )
+                )
+                // Mutationを実行する
+                .execute()
+                // レスポンスデータからbookを取得する（UpdateBookPayload）
+                .path("addBook")
+                .entity(UpdateBookPayload.class)
+                .satisfies(payload -> {
+                    // FIXME なぜかFalseが渡される。Trueしかセットしていないのに
+//                    Assert.assertTrue(payload.success());
+                    Assert.assertTrue(payload.book().name().equals("test"));
+                    Assert.assertTrue(payload.book().pageCount() == 528);
+                });
     }
 }
